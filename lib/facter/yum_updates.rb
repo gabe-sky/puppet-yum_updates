0797a99a8b86bbe8c12f286399cd3c138bd9ec16
Gabriel M Schuyler <gabe@puppetlabs.com>
2017-08-14 16:46:24 -0700

    Repair confinement to RedHat systems

--- lib/facter/yum_updates.rb
+++ lib/facter/yum_updates.rb
@@ -9,22 +9,23 @@
 Facter.add(:yum_updates) do
   confine :osfamily => 'RedHat'
 
-  results = Hash.new
-
-  # Invoke check-update to list available updates, use grep to exclude blank
-  # lines, and then count the number of remaining lines.
-  # Convert this from a string to an integer and assign it to 'count'.
-  results['count'] = Facter::Core::Execution.execute('yum check-update --quiet | grep -v "^$" | wc -l', :timeout => 30).to_i
-
-  # If the result was greater than zero, set 'avaialble' to true, to indicate
-  # that there are outstanding updates.  Otherwise set it to false.
-  if results['count'] > 0
-    results['available'] = true
-  else
-    results['available'] = false
-  end
-
   setcode do
+
+    results = Hash.new
+
+    # Invoke check-update to list available updates, use grep to exclude blank
+    # lines, and then count the number of remaining lines.
+    # Convert this from a string to an integer and assign it to 'count'.
+    results['count'] = Facter::Core::Execution.execute('yum check-update --quiet | grep -v "^$" | wc -l', :timeout => 30).to_i
+
+    # If the result was greater than zero, set 'avaialble' to true, to indicate
+    # that there are outstanding updates.  Otherwise set it to false.
+    if results['count'] > 0
+      results['available'] = true
+    else
+      results['available'] = false
+    end
+
     results
   end
 
